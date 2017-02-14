b0e308da8497492ed7dacee5cab2a1b2f2d62e33
The Old Nut Man <HuitronAJ@gmail.com>
2017-02-13 22:08:37 -0800

    Added method to check whether we are in designer mode.

--- Tanji/App.xaml.cs
+++ Tanji/App.xaml.cs
@@ -7,5 +7,10 @@ namespace Tanji
     public partial class App : Application
     {
         public static IMaster Master { get; set; }
+
+        public static bool InDesignMode()
+        {
+            return !(Current is App);
+        }
     }
 }
