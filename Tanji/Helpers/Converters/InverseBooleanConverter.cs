99e8721cfb19c7123b93de442151083f130756c0
ArachisH <HuitronAJ@gmail.com>
2017-02-21 14:02:41 -0800

    Implemented code for ConvertBack.

--- Tanji/Helpers/Converters/InverseBooleanConverter.cs
+++ Tanji/Helpers/Converters/InverseBooleanConverter.cs
@@ -7,15 +7,11 @@ namespace Tanji.Helpers.Converters
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (targetType != typeof(bool))
-                throw new InvalidOperationException("The target must be a boolean");
-
-            return !(bool)value;
+            return !((bool)value);
         }
-
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return !((bool)value);
         }
     }
 }
