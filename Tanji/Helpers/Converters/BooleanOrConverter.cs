d666c76418e2d90b188a9748ac2c2c047bff58c9
ArachisH <HuitronAJ@gmail.com>
2017-02-21 14:00:54 -0800

    Tried to make it look a bit cleaner.

--- Tanji/Helpers/Converters/BooleanOrConverter.cs
+++ Tanji/Helpers/Converters/BooleanOrConverter.cs
@@ -8,15 +8,14 @@ namespace Tanji.Helpers.Converters
         public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if (values.Length < 2)
+            {
                 return false;
+            }
 
-            var leftOr = (values[0] as bool?);
-            var rightOr = (values[1] as bool?);
-
-            return ((leftOr ?? false)
-                || (rightOr ?? false));
+            var left = ((values[0] as bool?) ?? false);
+            var right = ((values[1] as bool?) ?? false);
+            return (left | right);
         }
-
         public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
