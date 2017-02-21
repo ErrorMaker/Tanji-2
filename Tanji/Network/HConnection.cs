d0e251c6bf88aab880f07df3158a74b8faefa8fd
ArachisH <HuitronAJ@gmail.com>
2017-02-21 14:04:09 -0800

    Added Network namespace.

---
+++ Tanji/Network/HConnection.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Tangine.Network;
+using Tangine.Protocol;
+
+namespace Tanji.Network
+{
+    public class HConnection : IHConnection, IDisposable
+    {
+        private int _inSteps, _outSteps;
+        private readonly object _disposeLock;
+        private readonly object _disconnectLock;
+
+        /// <summary>
+        /// Occurs when the connection between the game client, and server have been intercepted.
+        /// </summary>
+        public event EventHandler Connected;
+        protected virtual void OnConnected(EventArgs e)
+        {
+            Connected?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Occurs when either the game client, or server have disconnected.
+        /// </summary>
+        public event EventHandler Disconnected;
+        protected virtual void OnDisconnected(EventArgs e)
+        {
+            Disconnected?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Occurs when the game client's outgoing data has been intercepted.
+        /// </summary>
+        public event EventHandler<DataInterceptedEventArgs> DataOutgoing;
+        protected virtual void OnDataOutgoing(DataInterceptedEventArgs e)
+        {
+            DataOutgoing?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Occrus when the server's incoming data has been intercepted.
+        /// </summary>
+        public event EventHandler<DataInterceptedEventArgs> DataIncoming;
+        protected virtual void OnDataIncoming(DataInterceptedEventArgs e)
+        {
+            DataIncoming?.Invoke(this, e);
+        }
+
+        private HotelEndPoint _remoteEndPoint;
+        public HotelEndPoint RemoteEndPoint
+        {
+            get
+            {
+                return (Remote?.EndPoint ?? _remoteEndPoint);
+            }
+        }
+
+        public HNode Local { get; private set; }
+        public HNode Remote { get; private set; }
+        public bool IsDisposed { get; private set; }
+        public bool IsConnected { get; private set; }
+
+        public HConnection()
+        {
+            _disposeLock = new object();
+            _disconnectLock = new object();
+        }
+
+        public Task InterceptAsync(IPEndPoint endpoint)
+        {
+            return InterceptAsync(new HotelEndPoint(endpoint));
+        }
+        public Task InterceptAsync(string host, int port)
+        {
+            return InterceptAsync(HotelEndPoint.Parse(host, port));
+        }
+        public async Task InterceptAsync(HotelEndPoint endpoint)
+        {
+            if (IsDisposed)
+            {
+                throw new ObjectDisposedException("Object has already been disposed.");
+            }
+
+            _remoteEndPoint = endpoint;
+            int interceptCount = 0;
+            while (!IsConnected)
+            {
+                HNode tempLocal = null;
+                HNode tempRemote = null;
+                try
+                {
+                    tempLocal = await HNode.AcceptAsync(endpoint.Port)
+                        .ConfigureAwait(false);
+
+                    if (++interceptCount == 2)
+                    {
+                        interceptCount = 0;
+                        continue;
+                    }
+                    byte[] buffer = await tempLocal.PeekBufferAsync(6)
+                        .ConfigureAwait(false);
+
+                    tempRemote = await HNode.ConnectNewAsync(endpoint)
+                        .ConfigureAwait(false);
+
+                    if (HResolver.Factory.AncientOut.GetHeader(buffer) == 206)
+                    {
+                        tempLocal.DataResolver = HResolver.Factory.AncientOut;
+                        tempRemote.DataResolver = HResolver.Factory.AncientIn;
+                    }
+                    else if (HResolver.Factory.Modern.GetHeader(buffer) == 4000)
+                    {
+                        tempLocal.DataResolver = HResolver.Factory.Modern;
+                        tempRemote.DataResolver = HResolver.Factory.Modern;
+                    }
+                    else
+                    {
+                        buffer = await tempLocal.ReceiveBufferAsync(32).ConfigureAwait(false);
+                        await tempRemote.SendAsync(buffer).ConfigureAwait(false);
+
+                        buffer = await tempRemote.ReceiveBufferAsync(1024).ConfigureAwait(false);
+                        await tempLocal.SendAsync(buffer).ConfigureAwait(false);
+
+                        continue;
+                    }
+
+                    Local = tempLocal;
+                    Remote = tempRemote;
+
+                    IsConnected = true;
+                    OnConnected(EventArgs.Empty);
+
+                    _outSteps = 0;
+                    Task interceptOutgoingTask = InterceptOutgoingAsync();
+
+                    _inSteps = 0;
+                    Task interceptIncomingTask = InterceptIncomingAsync();
+                }
+                finally
+                {
+                    if (Local != tempLocal)
+                    {
+                        tempLocal?.Dispose();
+                    }
+                    if (Remote != tempRemote)
+                    {
+                        tempRemote?.Dispose();
+                    }
+                }
+            }
+        }
+
+        public Task<int> SendToServerAsync(byte[] data)
+        {
+            return Remote.SendAsync(data);
+        }
+        public Task<int> SendToServerAsync(HPacket packet)
+        {
+            return Remote.SendAsync(packet.ToBytes());
+        }
+
+        public Task<int> SendToClientAsync(byte[] data)
+        {
+            return Local.SendAsync(data);
+        }
+        public Task<int> SendToClientAsync(HPacket packet)
+        {
+            return Local.SendAsync(packet.ToBytes());
+        }
+
+        private async Task InterceptOutgoingAsync()
+        {
+            HPacket packet = await Local.ReceivePacketAsync().ConfigureAwait(false);
+            if (packet != null)
+            {
+                var args = new DataInterceptedEventArgs(packet, ++_outSteps,
+                    true, InterceptOutgoingAsync, SendToServerAsync);
+
+                try { OnDataOutgoing(args); }
+                catch { args.Restore(); }
+
+                if (!args.IsBlocked && !args.WasRelayed)
+                {
+                    await SendToServerAsync(args.Packet)
+                        .ConfigureAwait(false);
+                }
+                if (!args.HasContinued)
+                {
+                    args.Continue();
+                }
+            }
+            else Disconnect();
+        }
+        private async Task InterceptIncomingAsync()
+        {
+            HPacket packet = await Remote.ReceivePacketAsync().ConfigureAwait(false);
+            if (packet != null)
+            {
+                var args = new DataInterceptedEventArgs(packet,
+                    ++_inSteps, false, InterceptIncomingAsync, SendToClientAsync);
+
+                try { OnDataIncoming(args); }
+                catch { args.Restore(); }
+
+                if (!args.IsBlocked && !args.WasRelayed)
+                {
+                    await SendToClientAsync(args.Packet)
+                        .ConfigureAwait(false);
+                }
+                if (!args.HasContinued)
+                {
+                    args.Continue();
+                }
+            }
+            else Disconnect();
+        }
+
+        public void Disconnect()
+        {
+            if (IsDisposed)
+            {
+                throw new ObjectDisposedException("Object has already been disposed.");
+            }
+            if (Monitor.TryEnter(_disconnectLock))
+            {
+                try
+                {
+                    if (Local != null)
+                    {
+                        Local.Dispose();
+                        Local = null;
+                    }
+                    if (Remote != null)
+                    {
+                        Remote.Dispose();
+                        Remote = null;
+                    }
+                    if (IsConnected)
+                    {
+                        IsConnected = false;
+                        OnDisconnected(EventArgs.Empty);
+                    }
+                }
+                finally { Monitor.Exit(_disconnectLock); }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            lock (_disposeLock)
+            {
+                if (IsDisposed) return;
+                if (disposing)
+                {
+                    Disconnect();
+                }
+                IsDisposed = true;
+            }
+        }
+    }
+}
---
+++ Tanji/Network/HNode.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+using Tangine.Network;
+using Tangine.Protocol;
+using Tangine.Protocol.Encryption;
+
+namespace Tanji.Network
+{
+    public class HNode : IDisposable
+    {
+        public bool IsConnected
+        {
+            get { return Client.Connected; }
+        }
+
+        public Socket Client { get; }
+        public HResolver DataResolver { get; set; }
+        public HotelEndPoint EndPoint { get; private set; }
+
+        public RC4 Encrypter { get; set; }
+        public bool IsEncrypting { get; set; }
+
+        public RC4 Decrypter { get; set; }
+        public bool IsDecrypting { get; set; }
+
+        public HNode()
+            : this(new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+        { }
+        public HNode(Socket client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+            Client = client;
+        }
+
+        public Task ConnectAsync(string host, int port)
+        {
+            return ConnectAsync(HotelEndPoint.Parse(host, port));
+        }
+        public async Task ConnectAsync(IPEndPoint endpoint)
+        {
+            EndPoint = (endpoint as HotelEndPoint);
+            if (EndPoint == null)
+            {
+                EndPoint = new HotelEndPoint(endpoint);
+            }
+
+            IAsyncResult result = Client.BeginConnect(endpoint, null, null);
+            await Task.Factory.FromAsync(result, Client.EndConnect).ConfigureAwait(false);
+        }
+        public Task ConnectAsync(IPAddress address, int port)
+        {
+            return ConnectAsync(new HotelEndPoint(address, port));
+        }
+        public Task ConnectAsync(IPAddress[] addresses, int port)
+        {
+            return ConnectAsync(new HotelEndPoint(addresses[0], port));
+        }
+
+        public Task<HPacket> PeekPacketAsync()
+        {
+            return ReceivePacketAsync(SocketFlags.Peek);
+        }
+        public Task<byte[]> PeekBufferAsync(int size)
+        {
+            return ReceiveBufferAsync(size, SocketFlags.Peek);
+        }
+
+        public Task<HPacket> ReceivePacketAsync()
+        {
+            return ReceivePacketAsync(SocketFlags.None);
+        }
+        public Task<byte[]> ReceiveBufferAsync(int size)
+        {
+            return ReceiveBufferAsync(size, SocketFlags.None);
+        }
+
+        public Task<int> SendAsync(byte[] buffer)
+        {
+            return SendAsync(buffer, buffer.Length);
+        }
+        public Task<int> SendAsync(byte[] buffer, int size)
+        {
+            return SendAsync(buffer, 0, size);
+        }
+        public Task<int> SendAsync(byte[] buffer, int offset, int size)
+        {
+            return SendAsync(buffer, offset, size, SocketFlags.None);
+        }
+
+        public Task<int> ReceiveAsync(byte[] buffer)
+        {
+            return ReceiveAsync(buffer, buffer.Length);
+        }
+        public Task<int> ReceiveAsync(byte[] buffer, int size)
+        {
+            return ReceiveAsync(buffer, 0, size);
+        }
+        public Task<int> ReceiveAsync(byte[] buffer, int offset, int size)
+        {
+            return ReceiveAsync(buffer, offset, size, SocketFlags.None);
+        }
+
+        public Task<int> PeekAsync(byte[] buffer)
+        {
+            return PeekAsync(buffer, buffer.Length);
+        }
+        public Task<int> PeekAsync(byte[] buffer, int size)
+        {
+            return PeekAsync(buffer, 0, size);
+        }
+        public Task<int> PeekAsync(byte[] buffer, int offset, int size)
+        {
+            return ReceiveAsync(buffer, offset, size, SocketFlags.Peek);
+        }
+
+        protected async Task<HPacket> ReceivePacketAsync(SocketFlags socketFlags)
+        {
+            if (DataResolver == null)
+            {
+                throw new NullReferenceException("Data resolver cannot be null.");
+            }
+
+            byte[] lengthData = await ReceiveBufferAsync(4, socketFlags)
+                .ConfigureAwait(false);
+
+            if (lengthData.Length == 0)
+            {
+                await DisconnectAsync().ConfigureAwait(false);
+                return null;
+            }
+
+            int msgLength = DataResolver.GetBodyLength(lengthData); //BigEndian.ToInt32(lengthData, 0);
+            var msgBody = new byte[msgLength];
+
+            int read = await ReceiveAsync(msgBody)
+                .ConfigureAwait(false);
+
+            int noneReadCount = 0;
+            while (read != msgLength)
+            {
+                int curRead = await ReceiveAsync(msgBody, read,
+                    (msgLength - read), socketFlags).ConfigureAwait(false);
+
+                if (!IsConnected ||
+                    (curRead == 0 && ++noneReadCount >= 2))
+                {
+                    await DisconnectAsync().ConfigureAwait(false);
+                    return null;
+                }
+                else if (curRead > 0)
+                {
+                    noneReadCount = 0;
+                    read += curRead;
+                }
+            }
+
+            var msgData = new byte[4 + msgLength];
+            Buffer.BlockCopy(lengthData, 0, msgData, 0, lengthData.Length);
+            Buffer.BlockCopy(msgBody, 0, msgData, lengthData.Length, msgBody.Length);
+
+            return null;
+            //return new HMessage(msgData);
+        }
+        protected async Task<byte[]> ReceiveBufferAsync(int size, SocketFlags socketFlags)
+        {
+            var buffer = new byte[size];
+            int read = await ReceiveAsync(buffer, 0, size, socketFlags).ConfigureAwait(false);
+
+            var trimmedBuffer = new byte[read];
+            Buffer.BlockCopy(buffer, 0, trimmedBuffer, 0, read);
+
+            return trimmedBuffer;
+        }
+        protected async Task<int> SendAsync(byte[] buffer, int offset, int size, SocketFlags socketFlags)
+        {
+            if (!IsConnected)
+            {
+                return 0;
+            }
+
+            if (IsEncrypting && Encrypter != null)
+            {
+                buffer = Encrypter.Parse(buffer);
+            }
+
+            IAsyncResult result = Client.BeginSend(buffer, offset, size, socketFlags, null, null);
+            return await Task.Factory.FromAsync(result, Client.EndSend).ConfigureAwait(false);
+        }
+        protected async Task<int> ReceiveAsync(byte[] buffer, int offset, int size, SocketFlags socketFlags)
+        {
+            if (!IsConnected)
+            {
+                return 0;
+            }
+
+            IAsyncResult result = Client.BeginReceive(buffer, offset, size, socketFlags, null, null);
+            int read = await Task.Factory.FromAsync(result, Client.EndReceive).ConfigureAwait(false);
+
+            if (read > 0 && IsDecrypting && Decrypter != null)
+            {
+                Decrypter.RefParse(buffer, offset, read,
+                    socketFlags.HasFlag(SocketFlags.Peek));
+            }
+            return read;
+        }
+
+        public async Task DisconnectAsync()
+        {
+            if (IsConnected)
+            {
+                try
+                {
+                    Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, false);
+                    Client.Shutdown(SocketShutdown.Both);
+
+                    IAsyncResult result = Client.BeginDisconnect(false, null, null);
+                    await Task.Factory.FromAsync(result, Client.EndDisconnect).ConfigureAwait(false);
+                }
+                catch (SocketException) { }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (IsConnected)
+                {
+                    Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, true);
+                    Client.Shutdown(SocketShutdown.Both);
+                }
+                Client.Close();
+            }
+        }
+
+        public static async Task<HNode> AcceptAsync(int port)
+        {
+            using (var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                listener.Bind(new IPEndPoint(IPAddress.Any, port));
+                listener.Listen(1);
+
+                IAsyncResult result = listener.BeginAccept(null, null);
+                Socket client = await Task.Factory.FromAsync(result, listener.EndAccept).ConfigureAwait(false);
+                return new HNode(client);
+            }
+        }
+        public static Task<HNode> ConnectNewAsync(string host, int port)
+        {
+            return ConnectNewAsync(HotelEndPoint.Parse(host, port));
+        }
+        public static async Task<HNode> ConnectNewAsync(IPEndPoint endpoint)
+        {
+            var node = new HNode();
+            await node.ConnectAsync(endpoint).ConfigureAwait(false);
+            return node;
+        }
+        public static Task<HNode> ConnectNewAsync(IPAddress address, int port)
+        {
+            return ConnectNewAsync(new HotelEndPoint(address, port));
+        }
+        public static Task<HNode> ConnectNewAsync(IPAddress[] addresses, int port)
+        {
+            return ConnectNewAsync(new HotelEndPoint(addresses[0], port));
+        }
+    }
+}
