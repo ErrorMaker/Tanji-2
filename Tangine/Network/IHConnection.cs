fa91abcfd34de5cd06cb19b236ad6686e822cb61
ArachisH <HuitronAJ@gmail.com>
2017-02-21 13:37:35 -0800

    Added Network namespace.

---
+++ Tangine/Network/IHConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+using Tangine.Protocol;
+
+namespace Tangine.Network
+{
+    public interface IHConnection
+    {
+        HotelEndPoint RemoteEndPoint { get; }
+
+        Task<int> SendToServerAsync(byte[] data);
+        Task<int> SendToServerAsync(HPacket packet);
+
+        Task<int> SendToClientAsync(byte[] data);
+        Task<int> SendToClientAsync(HPacket packet);
+    }
+}
