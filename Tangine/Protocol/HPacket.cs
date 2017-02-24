f6d278b501269956c0af64461af51f1e2d670c59
ArachisH <HuitronAJ@gmail.com>
2017-02-23 20:37:39 -0800

    Implemented the read functionality for packets.

--- Tangine/Protocol/HPacket.cs
+++ Tangine/Protocol/HPacket.cs
@@ -1,7 +1,16 @@
-﻿namespace Tangine.Protocol
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tangine.Protocol
 {
-    public class HPacket
+    [DebuggerDisplay("Header: {Header} | {ToString()}")]
+    public abstract class HPacket
     {
+        private byte[] _toBytesCache;
+        private string _toStringCache;
+
         private int _position;
         public int Position
         {
@@ -9,8 +18,35 @@
             set { _position = value; }
         }
 
-        public ushort Header { get; set; }
-        public int Length { get; private set; }
+        private ushort _header;
+        public ushort Header
+        {
+            get { return _header; }
+            set
+            {
+                if (_header != value)
+                {
+                    _header = value;
+                    ResetCache();
+                }
+            }
+        }
+
+        public List<byte> Body { get; }
+        public HResolver Resolver { get; }
+        public int ReadableBytes => GetReadableBytes(Position);
+
+        public HPacket(byte[] data, HResolver resolver)
+        {
+            Resolver = resolver;
+            Body = new List<byte>();
+            Header = resolver.GetHeader(data);
+            Body.AddRange(resolver.GetBody(data));
+
+            // We don't want to use original the byte array reference, since it could change outside of the class scope.
+            _toBytesCache = new byte[data.Length];
+            Buffer.BlockCopy(data, 0, _toBytesCache, 0, data.Length);
+        }
 
         public int ReadInt32()
         {
@@ -20,9 +56,11 @@
         {
             return ReadInt32(ref position);
         }
-        public int ReadInt32(ref int position)
+        public virtual int ReadInt32(ref int position)
         {
-            return 0;
+            int value = Resolver.ReadInt32(Body, position);
+            position += Resolver.GetSize(value);
+            return value;
         }
 
         public string ReadUTF8()
@@ -33,9 +71,11 @@
         {
             return ReadUTF8(ref position);
         }
-        public string ReadUTF8(ref int position)
+        public virtual string ReadUTF8(ref int position)
         {
-            return null;
+            string value = Resolver.ReadUTF8(Body, position);
+            position += Resolver.GetSize(value);
+            return value;
         }
 
         public bool ReadBoolean()
@@ -46,9 +86,11 @@
         {
             return ReadBoolean(ref position);
         }
-        public bool ReadBoolean(ref int position)
+        public virtual bool ReadBoolean(ref int position)
         {
-            return false;
+            bool value = Resolver.ReadBoolean(Body, position);
+            position += Resolver.GetSize(value);
+            return value;
         }
 
         public ushort ReadUInt16()
@@ -59,9 +101,11 @@
         {
             return ReadUInt16(ref position);
         }
-        public ushort ReadUInt16(ref int position)
+        public virtual ushort ReadUInt16(ref int position)
         {
-            return 0;
+            ushort value = Resolver.ReadUInt16(Body, position);
+            position += Resolver.GetSize(value);
+            return value;
         }
 
         public double ReadDouble()
@@ -72,9 +116,24 @@
         {
             return ReadDouble(ref position);
         }
-        public double ReadDouble(ref int position)
+        public virtual double ReadDouble(ref int position)
         {
-            return double.NaN;
+            double value = Resolver.ReadDouble(Body, position);
+            position += Resolver.GetSize(value);
+            return value;
+        }
+
+        public byte ReadByte()
+        {
+            return ReadByte(ref _position);
+        }
+        public byte ReadByte(int position)
+        {
+            return ReadByte(ref position);
+        }
+        public virtual byte ReadByte(ref int position)
+        {
+            return Body[position++];
         }
 
         public byte[] ReadBytes(int length)
@@ -85,17 +144,55 @@
         {
             return ReadBytes(length, ref position);
         }
-        public byte[] ReadBytes(int length, ref int position)
+        public virtual byte[] ReadBytes(int length, ref int position)
         {
-            return null;
-        }
-
-        public int GetReadableBytes(int position)
-        {
-            return 0;
+            var chunk = new byte[length];
+            for (int i = 0; i < length; i++)
+            {
+                chunk[i] = Body[position++];
+            }
+            return chunk;
         }
 
         public byte[] ToBytes()
+        {
+            if (_toBytesCache != null)
+            {
+                return _toBytesCache;
+            }
+            return (_toBytesCache = AsBytes());
+        }
+        public override string ToString()
+        {
+            if (_toStringCache != null)
+            {
+                return _toStringCache;
+            }
+            return (_toStringCache = AsString());
+        }
+        public int GetReadableBytes(int position)
+        {
+            return (Body.Count - position);
+        }
+
+        private void ResetCache()
+        {
+            _toBytesCache = null;
+            _toStringCache = null;
+        }
+        protected abstract byte[] AsBytes();
+        protected virtual string AsString()
+        {
+            string result = Encoding.Default.GetString(ToBytes());
+            for (int i = 0; i <= 13; i++)
+            {
+                result = result.Replace(
+                    ((char)i).ToString(), ("[" + i + "]"));
+            }
+            return result;
+        }
+
+        public static byte[] Construct(HResolver resolver, ushort header, params object[] values)
         {
             return null;
         }
