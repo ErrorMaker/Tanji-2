56fc753b26512ed618e9ffe3cbb870424b51b9ba
ArachisH <HuitronAJ@gmail.com>
2017-03-10 02:07:43 -0800

    Project file commit.

--- Tanji/Properties/AssemblyInfo.cs
+++ Tanji/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Windows;
+using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tanji")]
-[assembly: AssemblyDescription("Harble Traffic Manipulator")]
+[assembly: AssemblyDescription("Habbo Packet Analyzer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tanji")]
-[assembly: AssemblyCopyright("Copyright © HuitronAJ 2017")]
+[assembly: AssemblyCopyright("Copyright © ArachisH 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1775.0")]
-[assembly: AssemblyFileVersion("1.4.1774.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
